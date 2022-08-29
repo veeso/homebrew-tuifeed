c4ce408385c214a2ec5e771d0971dbfe3f80c7bb
veeso <christian.visintin1997@gmail.com>
2022-08-29 11:36:12 +0200

    0.3.0

--- Formula/tuifeed-m1.rb
+++ Formula/tuifeed-m1.rb
@@ -2,8 +2,8 @@ class TuifeedM1 < Formula
   desc "a terminal feed reader with a fancy ui"
   homepage "https://github.com/veeso/tuifeed"
   url "https://github.com/veeso/tuifeed/releases/latest/download/tuifeed-v0.2.1-arm64-apple-darwin.tar.gz"
-  sha256 "b018c122aaa0468a3e5bd9ed62a98e66b2c546102ffe09949f7b88cf6e7141dc"
-  version "0.2.1"
+  sha256 "6435dceac7fa82b7929088e8db32d246c0515e614881e114ac0c4d10ab43cc6c"
+  version "0.3.0"
 
   def install
     bin.install "tuifeed"
--- Formula/tuifeed.rb
+++ Formula/tuifeed.rb
@@ -2,8 +2,8 @@ class Tuifeed < Formula
   desc "a terminal feed reader with a fancy ui"
   homepage "https://github.com/veeso/tuifeed"
   url "https://github.com/veeso/tuifeed/releases/latest/download/tuifeed-v0.2.1-x86_64-apple-darwin.tar.gz"
-  sha256 "f2aad5f23d375309f5448fdca16aa7d9f0711819bc1609a544ce613c118af3a5"
-  version "0.2.1"
+  sha256 "353d0955f975a0b5c19f423ea7375e8edf188d41a09cbf064384b502e0a32dbb"
+  version "0.3.0"
 
   def install
     bin.install "tuifeed"
