8cf69cbfb377e3042f3e7eb9b5c59f15e3452b27
veeso <christian.visintin@veeso.dev>
2025-03-30 16:08:01 +0200

    put code that worked where the code that didn't used to be

--- Formula/tuifeed.rb
+++ Formula/tuifeed.rb
@@ -2,27 +2,27 @@ class Tuifeed < Formula
   desc "A terminal feed reader with a fancy ui"
   homepage "https://github.com/veeso/tuifeed"
   license "MIT"
-  version "0.4.1"
+  version "0.4.2"
 
   on_macos do
     on_arm do
-      url "https://github.com/veeso/tuifeed/releases/latest/download/tuifeed-v0.4.1-arm64-apple-darwin.tar.gz"
-      sha256 "17d6da314be7535c0994f9cef0df5251a961eff0598052f0d21e7478a26c705e"
+      url "https://github.com/veeso/tuifeed/releases/latest/download/tuifeed-v0.4.2-arm64-apple-darwin.tar.gz"
+      sha256 "9fda00690a6e75c06bc14a3544886ab9b5578de6878783b8bd6adbb659d714f9"
     end
     on_intel do
-      url "https://github.com/veeso/tuifeed/releases/latest/download/tuifeed-v0.4.1-x86_64-apple-darwin.tar.gz"
-      sha256 "c5dde4d28ac47e8f29c646101bd290e57aeefb9d2630fccc615c57a14561243c"
+      url "https://github.com/veeso/tuifeed/releases/latest/download/tuifeed-v0.4.2-x86_64-apple-darwin.tar.gz"
+      sha256 "360c9eb61297204d30c9633cf9fd41814c83247a3a1582c83bc5ba416050cca6"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/veeso/tuifeed/releases/latest/download/tuifeed-v0.4.1-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "811adf32128457cab3565742fcb9b0da39f11dbac002f98c54f6be6804b16d87"
+      url "https://github.com/veeso/tuifeed/releases/latest/download/tuifeed-v0.4.2-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "5549b776b315e7b5514e7638f7c90192d1bafff601105105ca8b5e4b253c20fa"
     end
     on_intel do
-      url "https://github.com/veeso/tuifeed/releases/latest/download/tuifeed-v0.4.1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "b320ed0fc0c5d9f5a5e9d91c6b432f489572ac941011ef62944a11839c37f8eb"
+      url "https://github.com/veeso/tuifeed/releases/latest/download/tuifeed-v0.4.2-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "75b0e2173ad491587ea986d9b18c5c45cbc70d0f6c2ed2930d569ea0bf67e9af"
     end
   end
 
